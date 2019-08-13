25326121b152bdc1b1a6c6e088e0473a0653be44
Linkfy <linkstudios@gmail.com>
2019-08-13 03:25:28 +0200

    Save cookies, states

--- login.js
+++ login.js
@@ -1,11 +1,56 @@
 require('dotenv').config();
+let fs = require('fs');
 let Bluebird = require('bluebird');
 let inquirer = require('inquirer');
 let Api = require('instagram-private-api');
 let _ = require('lodash');
 let ig = new Api.IgApiClient();
 
-console.log("Trying to log with " + process.env.IG_USERNAME);
+
+function saveCookies(cookies, state) {
+	//console.log(cookies);
+    //console.log(state);
+    var cookiepath = "cookies/" + process.env.IG_USERNAME + ".json";
+
+    if (!fs.existsSync(cookiepath)) {
+        //Create the file if it does not exists
+        fs.closeSync(fs.openSync(cookiepath,'w'));
+    } else {
+       //console.log("File exists on saveCookie function, do not create it again");
+    }
+    cookies.state = state;
+    cookies = JSON.stringify(cookies);
+    fs.writeFileSync(cookiepath, cookies);
+	return {
+		cookies,
+		state,
+	};
+}
+async function loadCookies() {
+    
+    var cookiepath = "cookies/" + process.env.IG_USERNAME + ".json";
+    console.log("Trying to load filepath " + cookiepath);
+    //console.log(__dirname);
+    if (fs.existsSync(cookiepath)) {
+        let cookies =  fs.readFileSync(cookiepath).toString();
+        //console.log(cookies);
+		// In order to restore session cookies you need this
+		await ig.state.deserializeCookieJar(cookies);
+        // In order to restore state we use this
+		cookies = JSON.parse(cookies);
+        ig.state.deviceString = cookies.state.deviceString;
+		ig.state.deviceId = cookies.state.deviceId;
+		ig.state.uuid = cookies.state.uuid;
+		ig.state.phoneId = cookies.state.phoneId;
+		ig.state.adid = cookies.state.adid;
+	    ig.state.build = cookies.state.build;
+        console.log("Cookies loaded");
+        return true;
+    }
+    console.log("No cookie file found in loadCookies function");
+    return false;
+}
+
 // You must generate device id's before login.
 // Id's generated based on seed
 // So if you pass the same value as first argument - the same id's are generated every time
@@ -13,13 +58,47 @@ ig.state.generateDevice(process.env.IG_USERNAME);
 // Optionally you can setup proxy url
 ig.state.proxyUrl = process.env.IG_PROXY;
 (async () => {
+    console.log("Trying to log with " + process.env.IG_USERNAME);
+    //First we check if the user have cookies
+    let hasCookies = await loadCookies();
+    
+
     // Execute all requests prior to authorization in the real Android application
-    await ig.simulate.preLoginFlow();
+	// This function executes after every request
+	ig.request.end$.subscribe(async () => {
+		// Here you have JSON object with cookies.
+		// You could stringify it and save to any persistent storage
+		const cookies = await ig.state.serializeCookieJar();
+		const state = {
+			deviceString: ig.state.deviceString,
+			deviceId: ig.state.deviceId,
+			uuid: ig.state.uuid,
+			phoneId: ig.state.phoneId,
+			adid: ig.state.adid,
+			build: ig.state.build,
+		};
+		saveCookies(cookies, state);
+		// In order to restore session cookies you need this
+		await ig.state.deserializeCookieJar(JSON.stringify(cookies));
+		ig.state.deviceString = state.deviceString;
+		ig.state.deviceId = state.deviceId;
+		ig.state.uuid = state.uuid;
+		ig.state.phoneId = state.phoneId;
+		ig.state.adid = state.adid;
+		ig.state.build = state.build;
+	});	
+
+	await ig.simulate.preLoginFlow();
     Bluebird.try( async() => {
-
-        const loggedInUser = await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
-        console.log(loggedInUser);
-
+        if(!hasCookies) {
+        console.log("User not logged in, login in");
+         const loggedInUser = await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
+         console.log(loggedInUser);
+        
+        }
+        let pk = await ig.user.getIdByUsername(process.env.IG_USERNAME);
+        const userFeed = ig.feed.user(pk);
+        console.log(userFeed);
     }).catch(Api.IgCheckpointError, async () => {
 
         console.log(ig.state.checkpoint);
@@ -39,5 +118,15 @@ ig.state.proxyUrl = process.env.IG_PROXY;
         
         let sendCode = await ig.challenge.sendSecurityCode(code);
         console.log(sendCode);
+    }).catch(Api.IgLoginRequiredError, () => {
+        if(hasCookies) {
+            console.log("Invalid cookies");
+        } else {
+            //This block is not supossed to be used never (IgLoginBadPasswordError) exists
+            console.log("Incorrect password");
+        }
+
+    }).catch(Api.IgLoginBadPasswordError, () => {
+        console.log("Incorrect password");
     });
 })();
