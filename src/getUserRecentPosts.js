6a135e84f15ccf2bd4179c5f692fe80367cc1037
Linkfy <linkstudios@gmail.com>
2019-09-13 10:31:07 +0200

    Get User Recent Posts and viewStories improvements

---
+++ src/getUserRecentPosts.js
@@ -0,0 +1,17 @@
+async function getUserRecentPosts(ig, username) {
+    let info = await getUserInfo(ig, username);
+    let feed = await ig.feed.user(info.pk);
+    let list = await feed.items();
+    //To keep getting content, maybe on a future function:
+    /* do {
+        moreList = await feed.items();
+        Array.prototype.push.apply(list,moreList);
+        console.log(list[list.length-1]);
+        console.log(list.length);
+       
+    } while(feed.moreAvailable == true); */
+    
+    return items;
+}
+
+module.exports = getUserRecentPosts;
