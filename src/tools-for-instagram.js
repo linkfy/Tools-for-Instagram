9d1a64d04c7e7e3adc9e40d87175daf143809512
Esteve Segura <estevesegura.gir@gmail.com>
2019-09-26 16:15:07 +0200

    feature added: upload picture
    The feature to upload photos has been added

--- src/tools-for-instagram.js
+++ src/tools-for-instagram.js
@@ -45,6 +45,7 @@ function toolsForInstagram() {
     global.requestLivestream = require('./imageHaveFaces.js');
     global.commentMediaId = require('./commentPost.js');
     global.getMediaIdInfo = require('./getMediaIdInfo.js');
+    global.uploadPicture = require('./uploadPicture');
 }
 
 module.exports = toolsForInstagram();
