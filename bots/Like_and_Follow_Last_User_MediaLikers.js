98f0bee7bb7424cbab3b7bd6703498eaf2b9f1e6
virginsince1997 <pikuda42438@gmail.com>
2019-11-28 03:44:40 -0600

    Like_and_Follow_Last_User_MediaLikers.js

---
+++ bots/Like_and_Follow_Last_User_MediaLikers.js
@@ -0,0 +1,31 @@
+require('./src/tools-for-instagram.js');
+
+(async () => {
+
+    console.log("\n -- TESTING --\n".bold.underline);
+
+    console.log("\n1 -- LOGIN --\n".bold.underline);
+    let ig = await login();
+	await setAntiBanMode(ig);
+
+    
+    
+    let likers = await getRecentPostLikersByUsername(ig, 'instagram');
+    // let likers = await getRecentPostLikers(ig, posts[0]);
+
+    for (let i = 0; i < likers.length; i++) {
+        try {
+            let liker = likers[i];
+            getFollowActivityByHours(ig, 24)
+            await followUser(ig, liker.username);
+			await sleep(60);
+			let medias = await getUserRecentPosts(ig, liker.username);
+			// store pictures of a liker in a variable 'medias'
+			await likePost(ig, medias[0]);
+			// like the most recent picture of a liker
+            await sleep(60);
+        } catch (error) {
+            await sleep(30);
+        }
+    }
+})();
