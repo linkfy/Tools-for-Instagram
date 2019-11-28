7d4329dc3cd7039dabcc017930e260a2ccea97e0
virginsince1997 <pikuda42438@gmail.com>
2019-11-28 03:37:57 -0600

    Unfollow_Everyone.js
    This script unfollows everyone in your following regardless if they are followed by this bot or not

---
+++ bots/Unfollow_Everyone.js
@@ -0,0 +1,25 @@
+require('./src/tools-for-instagram.js');
+
+
+(async () => {
+    console.log("\n-- : Unfollow Everyone --\n".bold.underline);
+    console.log("\n -- Script by: @virginsince1997 --\n".bold.underline);
+    let ig = await login();
+    await getFollowing(ig, ig.loggedInUser.username);
+    await sleep(60);
+    followings = await readFollowing(ig, ig.loggedInUser.username);
+    await sleep(4);
+    
+    for (let i = 0; i < followings.length; i++) {
+        try {
+            let following = followings[i];
+            await sleep(60);
+            await unfollowUser(ig, following.username);
+        } catch (e) {
+            console.log("Error", e.stack);
+            console.log("Error", e.name);
+            console.log("Error", e.message);
+            await sleep(30);
+        }
+    }
+})();
