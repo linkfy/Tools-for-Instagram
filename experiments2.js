5f2f37115203b3b55b4d0cb6162a34db75a646ec
Linkfy <linkstudios@gmail.com>
2020-02-17 17:29:36 +0100

    Updated Mqtt Repo

--- experiments2.js
+++ experiments2.js
@@ -2,11 +2,8 @@ require('./src/tools-for-instagram');
 
 (async () => {
 
-    getTelegramGroup();
-    //let ig = await login();
-    //let pictureUrl = "https://i.ytimg.com/vi/w6-1O0WCdGM/maxresdefault.jpg";
-    //let caption = "This is a test image that I'll delete in a few seconds";
-    //let image = await uploadPictureByUrl(ig, caption, pictureUrl);
     
+    let ig = await login();
+    console.log(await getUserInfo(ig, "instagram"));
     
 })();
